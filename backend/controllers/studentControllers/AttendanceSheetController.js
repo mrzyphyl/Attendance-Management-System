0f8dd846cc5ad10158b33c9214160180f84bdf4f
mrzyphyl <dispojobbsy@gmail.com>
2023-09-29 12:33:39 +0800

    added attendance controllers based on new schema

--- backend/controllers/studentControllers/AttendanceSheetController.js
+++ backend/controllers/studentControllers/AttendanceSheetController.js
@@ -1,2 +1,71 @@
 const asyncHandler = require('express-async-handler')
 const studentAttendance = require('../../models/studentModel/studentAttendanceModel')
+
+
+//Get One Attendance for Student User
+//@route GET /api/records/attendance/:id
+//@access Public
+const getOneAttendance = asyncHandler (async (req, res) => {
+    //Find the Student User record by ID
+    const record = await studentAttendance.findOne({
+        'attendance._id': req.params.id
+    })
+
+    if (!record) {
+        res.status(404); //Change the status to 404 for "Not Found"
+        throw new Error('Record not found');
+    }
+
+    //Find the specific Attendance record within the array
+    const attendanceItem = record.attendance.find(
+        (item) => item._id.toString() === req.params.id
+    )
+
+    if (!attendanceItem) {
+        res.status(404); // Change the status to 404 for "Not Found"
+        throw new Error('Item not found');
+    }
+
+    res.status(200).json(attendanceItem);
+})
+
+//Add Attendance for Student User
+//@route PUT /api/student-user/:id/attendance
+//@access Public
+const addAttendance = asyncHandler (async (req, res) => {
+    const { subject_code, subject_name, subject_time, subject_instructor } = req.body;
+
+    try {
+        // Find the patient record by ID
+        const record = await studentAttendance.findById(req.params.id);
+
+        if (!record) {
+            res.status(404); // Change the status to 404 for "Not Found"
+            throw new Error('Record not found');
+        }
+
+        // Create a new medical history item
+        const newAttendance = {
+            subject_code,
+            subject_name,
+            subject_time,
+            subject_instructor
+        };
+
+        // Add the new medical history item to the record
+        record.attendance.push(newAttendance);
+
+        // Save the updated record
+        await record.save();
+
+        res.status(201).json(newAttendance);
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
+})
+
+
+module.exports = {
+    getOneAttendance,
+    addAttendance
+}
--- backend/routes/studentRoutes/studentAttendanceRouter.js
+++ backend/routes/studentRoutes/studentAttendanceRouter.js
@@ -7,6 +7,10 @@ const {
     deltMultiStudentAttendance, 
     getMultipleStudentAttendance 
 } = require('../../controllers/studentControllers/studentAttendanceController')
+const { 
+    addAttendance, 
+    getOneAttendance 
+} = require('../../controllers/studentControllers/AttendanceSheetController')
 const router = express.Router()
 
 router.route('/').get(getStudentAttendance).post(postStudentAttendance)
@@ -14,5 +18,9 @@ router.route('/').get(getStudentAttendance).post(postStudentAttendance)
 router.route('/:id').delete(deltStudentAttendance).get(getOneStudentAttendance)
 
 router.route('/:ids').delete(deltMultiStudentAttendance).get(getMultipleStudentAttendance)
+
+router.route('/attendance/:id').get(getOneAttendance)
+
+router.route('/:id/attendance').post(addAttendance)
 
 module.exports = router
